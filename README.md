af8589edfd3b83051c30dc7627d8579666c157e6
Mike Luddy <mluddyjr@outlook.com>
2013-03-19 21:06:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ride With Me
+
+This is the initial version for
+[*Ride With Me*]
+
+by [Mike Luddy](mluddyjr@outlook.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rwm::Application.config.secret_token = '3903f68db27b9b690f2e8414775d53b2f2aafb376c08b8998d66b8fe1c69668584c8f5d9fbc9c46099e8cf42fb8448492a00c0ec01687bcc56abf7eddbe7cbc1'
